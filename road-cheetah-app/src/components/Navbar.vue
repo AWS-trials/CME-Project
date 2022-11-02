1a24da8b952d7f0e79b1905f05694c700e18765f
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-02 23:15:29 +0800

    Delivery table

--- road-cheetah-app/src/components/Navbar.vue
+++ road-cheetah-app/src/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >Logistics Management</a>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f3faff;">
+        <!-- <div class="container-fluid"> -->
+            <a class="navbar-brand nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >
+                <img src="../../public/favicon.png" alt="" width="40" height="24" class="d-inline-block align-text-top">
+                Logistics Management
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button> -->
+            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
@@ -22,7 +25,7 @@
                     </li> -->
                 </ul>
             </div>
-        </div>
+        <!-- </div> -->
     </nav>
 </template>
 
