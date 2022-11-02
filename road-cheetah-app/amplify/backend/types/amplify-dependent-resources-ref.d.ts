ac6b498eadf57d0ab379ba1d9f44615eb29f90b4
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-11-02 20:41:22 +0800

    added navbar

--- road-cheetah-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ road-cheetah-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "roadcheetahapp0d199ae7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
