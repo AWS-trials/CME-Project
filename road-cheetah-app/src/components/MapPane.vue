e96070a7fe723ca64efc3c9ed7637c89148b768b
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-03 01:12:19 +0800

    Map is added finally :")

---
+++ road-cheetah-app/src/components/MapPane.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class='mapPane'>
+        <div id='map'></div>
+    </div>
+</template>
+
+<script>
+    import { createMap, drawPoints } from 'maplibre-gl-js-amplify';
+
+    export default {
+        name: 'MapPane',
+        data() {
+            return {
+            }
+        },
+        created: async function () {
+            this.mapCreate();
+        },
+        methods: {
+            mapCreate: async function() {
+                const map = await createMap({
+                    container: 'map',
+                    center: [103.851959, 1.290270],
+                    zoom: 15,
+                    bearing: 64.8,
+                    pitch: 60,
+                    hash: true,
+                });
+
+                // map.on('load', function () {
+                //     drawPoints('pointsSource',
+                //         [
+                //             {
+                //                 coordinates: [139.7646, 35.6827],
+                //                 title: 'Point01',
+                //                 address: 'Main Points',
+                //             },
+                //             {
+                //                 coordinates: [139.7720, 35.6768],
+                //                 title: 'Point02',
+                //             },
+                //             {
+                //                 coordinates: [139.7607, 35.6759],
+                //             },
+                //         ],
+                //         map,
+                //         {
+                //             showCluster: true,
+                //             unclusteredOptions: {
+                //                 showMarkerPopup: true,
+                //                 defaultColor: '#005773'
+                //             },
+                //             clusterOptions: {
+                //                 showCount: true,
+                //                 fillColor: '#005773'
+                //             },
+                //         }
+                //     );
+                // });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #map {
+        z-index: 0;
+        height: 600px;
+    }
+</style>
--- road-cheetah-app/src/components/Navbar.vue
+++ road-cheetah-app/src/components/Navbar.vue
@@ -10,7 +10,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="nav nav-pills" style="color:black">
+                <ul class="nav navbar" style="color:black">
                     <li class="nav-item">
                         <router-link class="nav-link" to="/driverHomeView">My Deliveries</router-link>
                     </li>
