7579ad2c369c7cbf26015e4849e7294bf9f142a0
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-11-02 23:53:59 +0800

    added driverDeliveries table

---
+++ road-cheetah-app/src/views/DriverHomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>This is driver home view</h1>
+
+    <!-- My Deliveries -->
+    <DriverDeliveries/>
+</template>
+
+<script>
+import { DriverDeliveries } from "@/components/DriverDeliveries.vue";
+
+export default {
+    name: "DriverHomeView",
+    components: {
+        DriverDeliveries,
+    }
+};
+</script>
