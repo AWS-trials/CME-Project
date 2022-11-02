2bf44284ac820b656513f8a66f98a736c5d5cdcb
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-02 21:36:30 +0800

    UI updates

--- road-cheetah-app/src/components/Navbar.vue
+++ road-cheetah-app/src/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >Logistics Management</a>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            <a class="navbar-brand nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >
+                <img src="../../public/favicon.png" width="50" height="30" alt="">
+                Logistics Management
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button> -->
+            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
