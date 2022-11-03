c115383f3fa39a0e9d2a33d99bdbfd9fcaa48055
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-11-03 09:30:24 +0800

    driver home view with table and calculator done with mock data

--- road-cheetah-app/src/views/DriverHomeView.vue
+++ road-cheetah-app/src/views/DriverHomeView.vue
@@ -1,17 +1,16 @@
 <template>
-    <h1>This is driver home view</h1>
-
     <!-- My Deliveries -->
     <DriverDeliveries/>
 </template>
 
 <script>
-import { DriverDeliveries } from "@/components/DriverDeliveries.vue";
 
-export default {
+import DriverDeliveries from "@/components/DriverDeliveries.vue"
+
+export default{
     name: "DriverHomeView",
     components: {
         DriverDeliveries,
     }
-};
+}
 </script>
