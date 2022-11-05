eec428cc945c028b52912ee5a79a67aa313a87f1
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-05 15:24:18 +0800

    Updates to Delivery Home View page

--- road-cheetah-app/src/views/DriverHomeView.vue
+++ road-cheetah-app/src/views/DriverHomeView.vue
@@ -1,8 +1,12 @@
 <template>
     <!-- My Deliveries -->
-    <h1 class="pt-3">
-        <b>Welcome, user</b>
-    </h1>
+    <div class="container d-flex ms-auto pt-5">
+        <img class="mx-2" src="../../public/man.png" alt="" width="100" height="100">
+        <h1 class="pt-5 text-align-start">
+            <b>Welcome, user</b>
+        </h1>
+
+    </div>
     <DriverDeliveries/>
 </template>
 
