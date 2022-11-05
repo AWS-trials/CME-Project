bf00ab80c67843e879414bf6a79685bec9c67fe9
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-05 08:45:49 +0800

    connect driver home view UI to endpoint
    table dynamically populated from db

--- road-cheetah-app/src/components/DriverDeliveries.vue
+++ road-cheetah-app/src/components/DriverDeliveries.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class=" mt-5 p-3 mb-2 container bg-light ">
+    <!-- <div class=" mt-5 p-3 mb-2 container bg-light ">
         <h2 class="text-start">
             Delivery Progress
         </h2>
         <h5 class="text-start">
             {{ calculateCompletedDeliveries }} Orders are completed
         </h5>
-    </div>
+    </div> -->
 
     <br/>
 
@@ -20,18 +20,18 @@
                     <th scope="col">Delivery Address</th>
                     <th scope="col">Postal Code</th>
                     <th scope="col">Delivery Zone</th>
-                    <th scope="col">Delivery Status</th>
+                    <th scope="col">Current Delivery Status</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="order in orders">
-                    <th scope="row">{{ order.id }}</th>
-                    <td>{{ order.recipient}}</td>
-                    <td>{{ order.address }}</td>
-                    <td>{{ order.postalCode }}</td>
-                    <td>{{ order.zone }}</td>
-                    <td>
-                        <div class="dropdown">
+                    <th scope="row">{{ order.orderID }}</th>
+                    <td>{{ order.RecipientName}}</td>
+                    <td>{{ order.RecipientAddress }}</td>
+                    <td>{{ order.PostalCode }}</td>
+                    <td>{{ order.AssignedWarehouse}}</td>
+                    <td> {{ order.Status}}
+                        <!-- <div class="dropdown">
                             <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 {{ order.status }}
                             </button>
@@ -52,7 +52,7 @@
                                     </a>
                                 </li>
                             </ul>
-                        </div>
+                        </div> -->
                     </td>
                 </tr>
             </tbody>
@@ -60,36 +60,39 @@
     </div>
 </template>
 <script>
+import axios from 'axios';
+import moment from 'moment';
 export default {
     name: "DriverDeliveries",
     data(){
         return{
-            orders: [
-                {
-                    id: 1,
-                    recipient: "Mark",
-                    address: "Sembawang",
-                    postalCode: "123445",
-                    zone: "North",
-                    status: "Pending"
-                },
-                {
-                    id: 2,
-                    recipient: "Jacob",
-                    address: "Yishun",
-                    postalCode: "123445",
-                    zone: "North",
-                    status: "Pending"
-                },
-                {
-                    id: 3,
-                    recipient: "Larry the Bird",
-                    address: "Yishun",
-                    postalCode: "123445",
-                    zone: "North",
-                    status: "Pending"
-                }
-            ],
+            orders:[],
+            // orders: [
+            //     {
+            //         id: 1,
+            //         recipient: "Mark",
+            //         address: "Sembawang",
+            //         postalCode: "123445",
+            //         zone: "North",
+            //         status: "Pending"
+            //     },
+            //     {
+            //         id: 2,
+            //         recipient: "Jacob",
+            //         address: "Yishun",
+            //         postalCode: "123445",
+            //         zone: "North",
+            //         status: "Pending"
+            //     },
+            //     {
+            //         id: 3,
+            //         recipient: "Larry the Bird",
+            //         address: "Yishun",
+            //         postalCode: "123445",
+            //         zone: "North",
+            //         status: "Pending"
+            //     }
+            // ],
         }
     },
     components:{},
@@ -107,8 +110,34 @@ export default {
             this.numberCompletedDeliveries = completedDeliveries
         }
     },
-    computed: {
-        calculateCompletedDeliveries(){
+    created() {
+            var date = moment();
+            var currentDate = date.format('DD-MM-YYYY');
+            //console.log(currentDate);
+            var url = "https://6x9208fr4j.execute-api.ap-southeast-1.amazonaws.com/dev/DistributeOrderToEachDriver"
+            axios.get(url)
+			.then(response => {
+                var response = response.data
+                for(var key in response) {
+                    //console.log(response[key])
+                    var delivery_obj = response[key];
+                    //console.log(delivery_obj)
+                    for (let each in delivery_obj) {
+                        //console.log(typeof(delivery_obj[each]["AssignedDateTime"]),delivery_obj[each]["AssignedDateTime"])
+                        //console.log(typeof(currentDate),currentDate)
+                        if (delivery_obj[each]["AssignedDateTime"]==currentDate) {
+                            //console.log("woof")
+                            //console.group(delivery_obj[each])
+                            this.orders.push(delivery_obj[each])
+                        }
+                    }
+                }
+			})
+			.catch(error => {
+				// process error object
+			});
+            console.log("woof")
+            console.log(this.orders)
             let completedDeliveries = 0;
             for (let i = 0; i < this.orders.length; i++) {
                 if (this.orders[i].status === "Delivered") {
@@ -117,8 +146,7 @@ export default {
             }
             return completedDeliveries + "/" + this.orders.length
         }
-    },
-};
+    }
 </script>
 
 <style>
