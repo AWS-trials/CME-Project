91c85547a88873a4a5d055340e1b678be4d8cbc3
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-07 00:59:13 +0800

    Update Navbar.vue

--- road-cheetah-app/src/components/Navbar.vue
+++ road-cheetah-app/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f3faff;">
         <!-- <div class="container-fluid"> -->
-            <a class="navbar-brand nav-link" href="/driverHomeView" tabindex="-1" >
+            <a class="navbar-brand nav-link" href="/" tabindex="-1" >
                 <img src="../../public/favicon.png" alt="" width="40" height="24" class="d-inline-block align-text-top">
                 Logistics Management
             </a>
@@ -12,9 +12,9 @@
 
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="nav navbar" style="color:black">
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <router-link class="nav-link" to="/">Driver Deliveries</router-link>
-                    </li>
+                    </li> -->
                     <li class="nav-item">
                         <router-link class="nav-link" to="/driverRouteView">Delivery Route</router-link>
                     </li>
