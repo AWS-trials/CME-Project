63867d9f432c53381c3e6f238a02031207c259f4
curlinggg <curlingamal.2020@scis.smu.edu.sg>
2022-11-07 13:59:21 +0800

    Update Orders table

--- road-cheetah-app/src/components/allOrders.vue
+++ road-cheetah-app/src/components/allOrders.vue
@@ -19,33 +19,10 @@
                 <tr v-for="order in orders">
                     <th scope="row">#{{ order.orderID }}</th>
                     <td>{{ order.RecipientName}}</td>
-                    <td>{{order.DriverId}}</td>
+                    <td>{{order.AssignedDriver}}</td>
                     <td> {{ order.Status}} </td>
                     <td>{{ order.AssignedDateTime }}</td>
                     <td>{{ order.AssignedWarehouse}}</td>
-                        <!-- <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ order.status }}
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a class="dropdown-item" href="#" @click="updateDeliveryStatus(order,'Delivered')">
-                                        Delivered
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#" @click="updateDeliveryStatus(order,'In-Progress')">
-                                        In-Progress
-                                    </a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#" @click="updateDeliveryStatus(order,'Pending')">
-                                        Pending
-                                    </a>
-                                </li>
-                            </ul>
-                        </div> -->
-
                 </tr>
             </tbody>
         </table>
@@ -69,62 +46,24 @@ export default {
         updateDeliveryStatus(order, status){
             order.status = status
         },
-        // calculateCompletedDeliveries(orders){
-
-        //     let completedDeliveries = 0
-        //     for(let i = 0; i < this.orders.length; i++){
-        //         console.log(this.orders[i])
-        //         if(orders[i].status === "Delivered"){
-        //             completedDeliveries++
-        //         }
-        //     }
-        //     this.numberCompletedDeliveries = completedDeliveries
-        // }
     },
     async created() {
             var date = moment();
             var currentDate = date.format('DD-MM-YYYY');
             //console.log(currentDate);
-            var url = "https://6x9208fr4j.execute-api.ap-southeast-1.amazonaws.com/dev/DistributeOrderToEachDriver"
+            var url = "https://3mb16n3708.execute-api.ap-southeast-1.amazonaws.com/dev/orders"
             await axios.get(url)
 			.then(response => {
-                var response = response.data
-                console.log("here")
-                console.log(response.json)
-                for(var key in response) {
-                    //console.log(response[key])2
-                    var delivery_obj = response[key];
-                    //console.log(delivery_obj)
-                    //delivery_obj["driver_id"] = key;
-                    for (let each in delivery_obj) {
-                        //console.log(typeof(delivery_obj[each]["AssignedDateTime"]),delivery_obj[each]["AssignedDateTime"])
-                        //console.log(typeof(currentDate),currentDate)
-                        if (delivery_obj[each]["AssignedDateTime"]==currentDate) {
-                            //console.log("woof")
-                            // console.group(typeof(delivery_obj[each]))
-                            this.orders.push(delivery_obj[each])
-                        }
-                    }
-                    console.log(typeof(this.orders))
+                console.log(response.data)
+                var response = response.data.orders
+                for (let each in response) {
+                    console.log(response[each])
+                    this.orders.push(response[each])
                 }
 			})
 			.catch(error => {
 				// process error object
 			});
-            var totalDeliveries= this.orders.length;
-            var undelivered = 0;
-           // console.log(completedDeliveries)
-            let rawOrders = toRaw(this.orders)
-            rawOrders.forEach(o => 
-            {   console.log(o)
-                if (o["Status"] == "Undelivered") {
-                    console.log("dog",o)
-                    undelivered++;
-                }
-            })
-            this.numberInProgressDeliveries=undelivered
-            this.numberCompletedDeliveries=totalDeliveries-undelivered
-            // return completedDeliveries + "/" + this.orders.length
         }
     }
 </script>
